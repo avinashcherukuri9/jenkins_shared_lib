f1ff90717c194a1c31b95c9543a785067f3eeb0f
avinashcherukuri9 <avinash.cherukuri.9@gmail.com>
2024-02-07 19:55:20 +0530

    Create jfrogintergation.groovy

---
+++ vars/jfrogintergation.groovy
@@ -0,0 +1,14 @@
+def call(String project, String ImageTag, String hubUser){
+    withCredentials([usernamePassword(
+            credentialsId: "artifactory",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+
+      
+        def curlCommand = "curl -X PUT -u '${USER}:${PASS}' -T target/*.jar ${params.artifactoryURL}/artifactory/example-repo-local/"
+        echo "Executing curl command : $curlComamnd"
+        sh curlCommand
+    }
+      
+}
